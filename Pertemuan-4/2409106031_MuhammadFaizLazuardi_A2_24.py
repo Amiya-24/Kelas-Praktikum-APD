92617afd634dd2a81f45f0b30f1fa7b372a444e8
Amiya-24 <muhammadfaizlazuardi4@gmail.com>
2024-09-25 06:57:50 +0800

    Update 2409106031_MuhammadFaizLazuardi_A2_24.py

--- Pertemuan-4/2409106031_MuhammadFaizLazuardi_A2_24.py
+++ Pertemuan-4/2409106031_MuhammadFaizLazuardi_A2_24.py
@@ -1,9 +1,9 @@
 import os
 
-def bersihkan():
+def clean():
     os.system("cls")
 
-bersihkan() # dipakai untuk membersihkan tampilan terminal
+clean() # dipakai untuk membersihkan tampilan terminal
 
 
 #                                                           PENGULANGAN FOR
@@ -13,8 +13,9 @@ bersihkan() # dipakai untuk membersihkan tampilan terminal
 #     print("Perulangan ke-" + str(i))
 
 # mahasiswa = [2409106031, "Faiz", 0.31, True, "A2'24"]
-# for u in mahasiswa:
-#     print(u)
+# for i in mahasiswa:
+#     print(i) #menampilkan output secara bertahap (kebawah)
+# print(mahasiswa) #menampilkan output secara langsung (kekanan)
 
 # for i in range(5, 11): # dimulai dari 5 dan berakhir di 10
 #     for j in range(1, 11): # dimulai dari 1 dan berakhir di 10
@@ -29,7 +30,7 @@ bersihkan() # dipakai untuk membersihkan tampilan terminal
 #     print("Yakin Ingin Mengulang (Y/N)")
 #     hitung += 1
 #     jawab = input("yakin ngulang? ")
-#     bersihkan()
+#     clean()
 # print(f"total pengulangan: {hitung}")
 
 # hitung = 0
@@ -37,26 +38,26 @@ bersihkan() # dipakai untuk membersihkan tampilan terminal
 #     print(f"Pengulangan ke-{hitung}")
 #     hitung += 1
 #     ulang = input("Masih Ingin Mengulang? ")
-#     bersihkan()
+#     clean()
 #     if ulang == "tidak" or ulang == "Tidak" or ulang == "ga" or ulang == "Ga" or ulang == "g" or ulang == "G" or  ulang == "":
 #         break
 # print(f"Total Perulangan: {hitung}")
 
 # print("Daftar bilangan genap dari 1-10")
 # for i in range(1, 11): # dimulai dari 1 dan berakhir di 10
-#     if i % 2 == 1: # kalau memenuhi syarat maka dia dilewati, jika beda maka dia akan di print
-#         continue
+#     if i % 2 == 1: 
+#         continue # kalau memenuhi syarat maka dia dilewati, jika beda maka dia akan di print
 #     print(i)
 
 
 #                                                             Studi Kasus 2
-total = 0
-angka = 0
+# total = 0
+# angka = 0
 
-while angka >= 0:
-    angka = int(input("Masukkan Angka: "))
-    if angka < 0:
-        break
-    total += angka
+# while angka >= 0:
+#     angka = int(input("Masukkan Angka: "))
+#     if angka < 0:
+#         break
+#     total += angka
 
-print("Jumlah total dari inputan adalah:", total)
+# print("Jumlah total dari inputan adalah:", total)

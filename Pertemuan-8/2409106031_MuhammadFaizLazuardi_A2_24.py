6477329b7c7d1801d21bf0ad7922f83d21e7b80c
Amiya-24 <muhammadfaizlazuardi4@gmail.com>
2024-10-23 07:45:49 +0800

    Update 2409106031_MuhammadFaizLazuardi_A2_24.py

--- Pertemuan-8/2409106031_MuhammadFaizLazuardi_A2_24.py
+++ Pertemuan-8/2409106031_MuhammadFaizLazuardi_A2_24.py
@@ -6,53 +6,53 @@ def clean():
 clean() # dipakai untuk membersihkan tampilan terminal
 
 
-# #                   ValueError
-# angka = int(input("Masukkan angka: ")) #input “Hello”
-# print(angka)
+#                   ValueError
+angka = int(input("Masukkan angka: ")) #input “Hello”
+print(angka)
 
 
-# #                   Try-Except
-# try: # dicoba dulu di try, jika terjadi error maka akan langsung masuk ke except
-#     angka = int(input("Masukkan angka: "))
+#                   Try-Except
+try: # dicoba dulu di try, jika terjadi error maka akan langsung masuk ke except
+    angka = int(input("Masukkan angka: "))
 
-# except ValueError: # except ... (bisa error apa saja, tidak pasti ValueError)
-#     print("Input yang anda masukkan bukan angka")
-
-
-# #                 Try-Except-Else
-# try: # dicoba dulu di try, jika terjadi error maka akan langsung masuk ke except
-#     angka = int(input("Masukkan angka: "))
-
-# except ValueError: # except ... (bisa error apa saja, tidak pasti ValueError)
-#     print("Input yang anda masukkan bukan angka")
-
-# else: # akan dijalankan jika tidak terjadi error pada try
-#     print(f"Angka yang kamu input: {angka}")
+except ValueError: # except ... (bisa error apa saja, tidak pasti ValueError)
+    print("Input yang anda masukkan bukan angka")
 
 
-# #             Try-Except-Else-Finally
-# try: # dicoba dulu di try, jika terjadi error maka akan langsung masuk ke except
-#     angka = int(input("Masukkan angka: "))
+#                 Try-Except-Else
+try: # dicoba dulu di try, jika terjadi error maka akan langsung masuk ke except
+    angka = int(input("Masukkan angka: "))
 
-# except ValueError: # except ... (bisa error apa saja, tidak pasti ValueError)
-#     print("Input yang anda masukkan bukan angka")
+except ValueError: # except ... (bisa error apa saja, tidak pasti ValueError)
+    print("Input yang anda masukkan bukan angka")
 
-# else: # akan dijalankan jika tidak terjadi error pada try
-#     print(f"Angka yang kamu input: {angka}")
-
-# finally: # akan selalu dijalankan walau program mengalami error
-#     print("Program selesai")
+else: # akan dijalankan jika tidak terjadi error pada try
+    print(f"Angka yang kamu input: {angka}")
 
 
-# #                   kustom Error
-# try:
-#     nama = input("Hello, what's your name? ")
-#     if len(nama) > 5:
-#         raise ValueError("Nama tidak boleh lebih dari 5 karakter")
-# # dengan menggunakan 'raise' kita dapat menyesuaikan output apa yang akan keluar jika terdapat error
+#             Try-Except-Else-Finally
+try: # dicoba dulu di try, jika terjadi error maka akan langsung masuk ke except
+    angka = int(input("Masukkan angka: "))
 
-# except ValueError as e: # tidak mesti menggunakan 'e'
-#     print(e)
+except ValueError: # except ... (bisa error apa saja, tidak pasti ValueError)
+    print("Input yang anda masukkan bukan angka")
+
+else: # akan dijalankan jika tidak terjadi error pada try
+    print(f"Angka yang kamu input: {angka}")
+
+finally: # akan selalu dijalankan walau program mengalami error
+    print("Program selesai")
+
+
+#                   kustom Error
+try:
+    nama = input("Hello, what's your name? ")
+    if len(nama) > 5:
+        raise ValueError("Nama tidak boleh lebih dari 5 karakter")
+# dengan menggunakan 'raise' kita dapat menyesuaikan output apa yang akan keluar jika terdapat error
+
+except ValueError as e: # tidak mesti menggunakan 'e'
+    print(e)
 
 
 
